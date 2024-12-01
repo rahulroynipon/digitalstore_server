1441fee48dc5ef8e1ec9e418c3c434aa3c3ea734
rahulroynipon <rahulroynipon@gmail.com>
2024-12-02 02:29:40 +0600

    added the start command

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN.split(","),
+        origin: process.env.CORS_ORIGIN,
         credentials: true,
         optionsSuccessStatus: 200,
     })
