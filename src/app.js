7ba27d04768cdba6efd1272e5a35ea6e1b3af1f6
rahulroynipon <rahulroynipon@gmail.com>
2024-12-02 21:56:57 +0600

    added for deployment

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN,
+        origin: process.env.CORS_ORIGIN.split(','),
         credentials: true,
         optionsSuccessStatus: 200,
     })
