37af5856b116a8fd1c84ba7f05a8c8cebabefa9b
rahulroynipon <rahulroynipon@gmail.com>
2024-12-03 03:06:22 +0600

    added the commit

--- src/app.js
+++ src/app.js
@@ -4,35 +4,13 @@ import cookieParser from "cookie-parser";
 import { LIMIT } from "./constants.js";
 import { ApiError } from "./utils/ApiError.js";
 import admin from "firebase-admin";
-<<<<<<< HEAD
 import { serviceAccount } from "./constants.js";
-=======
-
-const serviceAccount = {
-    type: process.env.SERVICE_ACCOUNT_TYPE,
-    project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
-    private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-    private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
-    client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
-    auth_uri: process.env.SERVICE_ACCOUNT_AUTH_URI,
-    token_uri: process.env.SERVICE_ACCOUNT_TOKEN_URI,
-    auth_provider_x509_cert_url:
-        process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_CERT_URL,
-    client_x509_cert_url: process.env.SERVICE_ACCOUNT_CLIENT_CERT_URL,
-    universe_domain: process.env.UNIVERSE_DOMAIN,
-};
->>>>>>> 7ba27d04768cdba6efd1272e5a35ea6e1b3af1f6
 
 const app = express();
 
 app.use(
     cors({
-<<<<<<< HEAD
         origin: process.env.CORS_ORIGIN,
-=======
-        origin: process.env.CORS_ORIGIN.split(','),
->>>>>>> 7ba27d04768cdba6efd1272e5a35ea6e1b3af1f6
         credentials: true,
         optionsSuccessStatus: 200,
     })
