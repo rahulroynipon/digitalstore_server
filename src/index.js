02af6eef1e574c4de7c45e31f357c87a2a2c8235
rahulroynipon <rahulroynipon@gmail.com>
2024-12-03 19:50:26 +0600

    added the production confi

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ connectDB()
         app.listen(port, () => {
             console.log("The portal hosted at port:", port);
         });
+
+        app.get("/", (req, res) => {
+            return res.send("<h1>This is the API for the Digital Store</h1>");
+        });
     })
     .catch((error) => {
         console.log("MONGODB connection is failed", error);
