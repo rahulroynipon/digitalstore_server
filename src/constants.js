3a0df5d93294fe8781715f83df565d46a8b38b73
rahulroynipon <rahulroynipon@gmail.com>
2024-12-04 15:48:28 +0600

    added the dommain

--- src/constants.js
+++ src/constants.js
@@ -20,8 +20,9 @@ export const COOKIE_OPTIONS = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "None",
-    path: "/", 
+    path: "/",
     maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    domain: process.env.CORS_ORIGIN,
 };
 
 export const MAINTENANCE_DAY = 15;
